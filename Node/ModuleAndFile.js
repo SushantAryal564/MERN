200c6098ef3f47dc7c273142bc307cbef6b10113
sushant <aryalsushant564@gmail.com>
2022-08-10 15:59:43 +0545

    React revision on useState

---
+++ Node/ModuleAndFile.js
@@ -0,0 +1,43 @@
+// console.log("Hello")
+// console.log("Sushant")
+// const FirstTask = ()=>{
+//   setTimeout(()=>{
+//     console.log("Holy fuck");
+//   },5000)
+//   console.log("First task called");
+// }
+// const secondTask = () => {
+//   console.log("Second task called");
+// };
+// FirstTask()
+// secondTask()
+
+// global.console.log("hi");
+
+// global.setTimeout(() => {
+//   console.log("Hi sushant Why are you so lonely.")
+// }, 3000);
+
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(global);
+
+const os = require('os');
+// console.log(os.arch());
+// console.log(os.cpus());
+// console.log(os.freemem());
+// console.log(os.hostname());
+// console.log(os.homedir());
+// console.log(os.hostname());
+// console.log(os.loadavg());
+// console.log(os.networkInterfaces());
+// console.log(os.uptime());
+// console.log(os.userInfo());
+// console.log(os.platform());
+
+const {firstData, secondData} = require("./data.js")
+console.log(firstData);
+console.log(secondData);
+
+
+
---
+++ Node/data.js
@@ -0,0 +1,10 @@
+const data1 = ["fi","bye",5.0];
+const data2 = ["rajesh","hamal"];
+const obj = {
+  name:"Sushant",
+  cast: "Aryal",
+}
+module.exports = {
+  firstData: data1,
+  secondData:data2,
+}
